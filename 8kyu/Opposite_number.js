745d9cfd4a974f0d1500c4c7d3fbd986cf41d073
Peter Kim <seongjooon@gmail.com>
2019-03-10 19:58:38 +0900

    complete Opposite_number

---
+++ 8kyu/Opposite_number.js
@@ -0,0 +1,14 @@
+/* Opposite number
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34 */
+
+const number = -34;
+
+const opposite = number => -number
+
+console.log(opposite(number))
