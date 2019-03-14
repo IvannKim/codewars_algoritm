b0b76cb7e2e0b4ddc3f9cff2b2c3ce4746b41891
Peter Kim <seongjooon@gmail.com>
2019-03-14 17:55:35 +0900

    재귀함수 주찬이랑 품

---
+++ 6ku/Find-the-odd-int.js
@@ -0,0 +1,24 @@
+/* Find the odd int
+
+Given an array, find the int that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+*/
+
+const arr = [5, 4, 3, 2, 9, 5, 4, 3, 2, 10, 10]
+
+function findOdd(arr) {
+    let count = 0;
+    arr.sort((a, b) => a - b)
+    console.log(arr)
+    let arr1 = arr[0];
+    arr.forEach((element) => {
+        if (arr1 === element) count++
+    })
+    if (count % 2 === 0) {
+        arr.splice(0, count)
+        findOdd(arr);
+    } else return arr1
+}
+
+console.log(findOdd(arr))
