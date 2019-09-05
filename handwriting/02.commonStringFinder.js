5835b78bf0c812c9f1a94420521aaa9b4ca61e08
MrKamake <seongjooon@gmail.com>
2019-09-05 15:40:57 +0900

    update handwriting/02.commonStringFinder.js

--- handwriting/02.commonStringFinder.js
+++ handwriting/02.commonStringFinder.js
@@ -1,13 +1,17 @@
 const a = 'abca';
-const b = 'baa';
+const b = 'baaaaaaa';
 
 const commonStringFinder = (a, b) => {
+  if (!a.length || !b.length) return '';
+  
   const splitedA = a.split('');
-  let splitedB = b.split('');
+  const splitedB = b.split('');
 
-  return splitedA.reduce((acc, cur) => {
-    if (splitedB.indexOf(cur) !== -1) acc += cur;
-    splitedB = splitedB.filter(e => splitedB[splitedB.indexOf(cur)] !== e);
+  return splitedA.reduce((acc, cur, idx) => {
+    const findIndex = splitedB.indexOf(cur);
+    const isFirstIndex = splitedA.indexOf(cur);
+
+    if (findIndex !== -1 && isFirstIndex === idx) acc += cur;
 
     return acc;
   }, '');
