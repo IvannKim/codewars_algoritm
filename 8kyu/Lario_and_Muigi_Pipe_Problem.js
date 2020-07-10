997b6fe3dbcf53a667b8311e21dfff84507d25a0
seongjoon <seongjooon@gmail.com>
2020-07-10 13:27:09 +0900

    add

--- 8kyu/Lario_and_Muigi_Pipe_Problem.js
+++ 8kyu/Lario_and_Muigi_Pipe_Problem.js
@@ -1,4 +1,6 @@
-/*#Issue Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+/*Lario and Muigi Pipe Problem
+
+#Issue Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
 
 The pipes connecting your level's stages together need to be fixed before you recieve any more complaints. Each pipe should be connecting, since the levels ascend, you can assume every number in the sequence after the first index will be greater than the previous and that there will be no duplicates.
 
